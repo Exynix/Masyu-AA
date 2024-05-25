afc18dfe699588549cef5f117df3356a2a6bd161
David <davids_barretom@javeriana.edu.co>
2024-05-24 20:27:23 -0500

    First scaffold of project. Creation on HTML, CSS files

--- Web_Masyu/Masyu_Game/apps.py
+++ Web_Masyu/Masyu_Game/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class MasyuGameConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "polls"
+    name = "Masyu_Game"
--- Web_Masyu/Web_Masyu/wsgi.py
+++ Web_Masyu/Web_Masyu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_Masyu project.
+WSGI config for Web_Masyu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Masyu.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web_Masyu.settings")
 
 application = get_wsgi_application()
